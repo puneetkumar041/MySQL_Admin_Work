e61a6b3c85e114a6c5a8b8f7731a2c092c033683
Puneet kumar <puneetkumar041@gmail.com>
2023-12-11 15:29:10 +0530

    Add files via upload
    Day to day google alert scripts

---
+++ DBA_daily_scripts.sh
@@ -0,0 +1,254 @@
+
+#!/bin/bash
+
+####################################################################################################
+# USER Backup in MySQL .
+####################################################################################################
+
+
+set -x
+BACKUP_DIR="/home/dba/usersync"
+MYSQL_USER="root"
+MYSQL_PASSWORD="m@nwiththepl@n"
+MYSQL=/usr/local/mysql/bin/mysql
+echo -n "Enter Socket: "
+read socket_name
+echo -n "Enter Hostname: "
+read host_name
+cd $BACKUP_DIR
+$MYSQL -u$MYSQL_USER -p$MYSQL_PASSWORD -S$socket_name -s -N -e"select concat('show grants for \'',user,'\'@\'',host,'\';') from mysql.user where user not in('mafiree','root','repl','replpuser','replusr','replusrnew','appuser');" > $BACKUP_DIR/import.sql 2> bkp.err
+$MYSQL -u$MYSQL_USER -p$MYSQL_PASSWORD -S$socket_name -s -N -e"select distinct(host) from mysql.user where user not in ('mafiree','repluser','root','repl','replpuser','replusr','replusrnew','appuser');" > $BACKUP_DIR/distincthost.txt
+$MYSQL -u$MYSQL_USER -p$MYSQL_PASSWORD -S$socket_name -s -N < $BACKUP_DIR/import.sql > grant.log 1> grant.sql 2> grant.err
+cat $BACKUP_DIR/distincthost.txt | while read LINE
+do
+sed -i "s/${LINE}/${host_name}/g" grant.sql
+done
+sed -i "s/$/;/g" grant.sql
+exit 0
+
+
+
+####################################################################################################
+# Health_Chhek
+####################################################################################################
+
+
+!/bin/sh
+set -x
+date=`date '+%Y_%m_%d'`
+
+/usr/local/mysql/bin/mysql -u maf_stats_coll -p'st@ts_c0ll' -h10.10.5.140 -P3306 -e"select now();" >> /home/dba/master_health_check/master_health_${date}.txt
+/usr/local/mysql/bin/mysqladmin ping -u maf_stats_coll -p'st@ts_c0ll' -h10.10.5.140  -P3306 >> /home/dba/master_health_check/master_health_${date}.txt
+exit 0
+
+
+
+
+
+#!/bin/bash
+
+####################################################################################################
+# Checks MyISAM tables in MySQL . Sends user(s) a notification when found                          #
+####################################################################################################
+
+
+DBHost=`cat /home/puneetkumar/cred.txt  | grep 'DST_HOST' | cut -d '"' -f 2`
+DBUser=`cat /home/puneetkumar/cred.txt | grep 'DST_USER' | cut -d '"' -f 2`
+DBPwd=`cat /home/puneetkumar/cred.txt | grep 'DST_PASSWORD' | cut -d '"' -f 2`
+
+
+
+# Function to send message to Google Chat
+send_message_to_google_space() {
+  local webhook_url="https://chat.googleapis.com/v1/spaces/AAAAmfYNxN8/messages?key=AIzaSyDdI0hCZtE6vySjMm-WEfRq3CPzqKqqsHI&token=AQqZhybd_r9SKwj2jf5g5yUvnR8FvXyH7Z-B9bBsyRM"
+  local message="$1"
+
+  curl -X POST -H "Content-Type: application/json" -d "{\"text\": \"$message\"}" "$webhook_url"
+}
+
+alert_message="📢 MyISAM ENGINE  VERIFY  📢"
+
+
+alert_message+="\n\n"
+###################################################################################
+TableCount=`/usr/local/mysql/bin/mysql -h$DBHost -u$DBUser -p$DBPwd  -e "select count(table_name) as cnt from information_schema.tables where ENGINE='MyISAM' and TABLE_TYPE='BASE TABLE' and table_schema not in ('information_schema','mysql','sys','performance_schema') ;"| tail -n 1`
+/usr/local/mysql/bin/mysql -h$DBHost -u$DBUser -p$DBPwd  -e "select group_concat(concat(' ',table_Schema ,\".\",table_name,\"   \") )  from information_schema.tables where ENGINE='MyISAM' and TABLE_TYPE='BASE TABLE' and table_schema not in ('information_schema','mysql','sys','performance_schema') ;"> /tmp/check.out
+list=`cat /tmp/check.out | tail -n 1`
+
+if [ "$TableCount" = "0" ]; then
+
+error="NO MyISAM tables found on  MySQL server : ($DBHost) "
+status=1
+fi
+
+if [ "$TableCount" -gt "0" ]; then
+    error="MyISAM Tables list on MySQL server : ($DBHost) is :: $list"
+        status=1
+fi
+
+
+##########################################
+# If found table list                   #
+##########################################
+
+if [ $status = 1 ]; then
+
+      send_alert=1
+alert_message+=" $error"
+fi
+
+if [[ $send_alert -eq 1 ]];
+then
+    send_message_to_google_space "$alert_message"
+fi
+
+
+0 10 * * * /bin/bash /root/mysql_table_engine_verify.sh 
+
+
+
+
+-- DISK USAGE
+
+project.txt
+prod
+
+
+                          cat disk_alert.sh
+df -PkH | grep -vE '^Filesystem|tmpfs|cdrom' | awk '{ print $5 " " $6 }' | while read output;
+do
+  usep=$(echo $output | awk '{ print $1}' | cut -d'%' -f1 )
+  partition=$(echo $output | awk '{print $2}' )
+  if [ $usep -ge 95 ]; then
+
+    msg="Running out of disk space $partition $(hostname) - amazon - 172.31.33.26 ($usep%)"
+    curl -X POST --data-urlencode "payload={\"channel\": \"#newrelic_disk_full\", \"username\": \"disk_script\", \"text\": \"Alert: $msg\", \"icon_emoji\": \":hankey:\"}" https://hooks.slack.com/services/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
+  fi
+done
+
+
+
+cat diskUsageAlert.sh
+#!/bin/bash
+
+# Function to send message to Google Chat
+send_message_to_google_space() {
+  local webhook_url="https://chat.googleapis.com/v1/spaces/AAAAMTVpo2A/messages?key=AIzaSyDdI0hCZtE6vySjMm-WEfRq3CPzqKqqsHI&token=r0mtPLmoMFjWVJWYTUTnmtuAlBiSUH5lz3Bsj5UVYww"
+  local message="$1"
+
+  curl -X POST -H "Content-Type: application/json" -d "{\"text\": \"$message\"}" "$webhook_url"
+}
+
+threshold=85
+send_alert=0
+
+alert_message="\u23F0 Disk Usage Alert \u23F0"
+alert_message+="\n\nHost IP : $(hostname -I | cut -d' ' -f1) (MySQL)"
+
+# Get all the mounted disk paths
+mounted_paths=($(df -h | grep -vE 'tmpfs|cdrom' | awk '{if (NR>1) print $6}'))
+
+# Get the IP address of the current machine
+ip_address=$(hostname -I | cut -d' ' -f1)
+
+# Loop through the mounted disk paths and check disk usage
+for path in "${mounted_paths[@]}"; do
+  full_path=$(df -h "$path" | awk '{if (NR>1) print $0}' )
+  usep=$(echo $full_path | awk '{if (NR>0) print $5}' | cut -d'%' -f1)
+  if [[ $usep -gt $threshold ]];
+  then
+      send_alert=1
+      alert_message+="\n$full_path \u26A0"
+  fi
+done
+
+if [[ $send_alert -eq 1 ]];
+then
+    send_message_to_google_space "$alert_message"
+fi 
+
+0 */12 * * * /bin/bash /root/diskUsageAlert.sh
+
+
+-- replication alerts
+
+CREATE USER 'db_script_user'@'%' IDENTIFIED BY 'SMxrx@st!3ey%Us<8';
+GRANT all ON *.* TO 'db_script_user'@'%' WITH GRANT OPTION;
+GRANT select , show databases  ON *.* TO 'db_script_user'@'%';
+FLUSH PRIVILEGES;
+
+*/10  * * * * /bin/bash /home/puneetkumar/mysql_replication_alert.sh > /home/puneetkumar/logfile.log 2>&1
+
+vim /home/puneetkumar/cred.txt
+SRC_HOST="nexs-db-master.prod.internal"
+SRC_USER="db_script_user"
+SRC_PASSWORD="Ad^s7<mE@WGt%s!8"
+DST_HOST="nexs-slave-db.prod.internal"
+DST_USER="db_script_user"
+DST_PASSWORD="SMxrx@st!3ey%Us<8"
+
+
+vim mysql_replication_alert.sh
+#!/bin/bash
+
+####################################################################################################
+# Checks MySQL Replication status. Sends user(s) a notification when the replication goes down     #
+####################################################################################################
+
+status=0
+SlaveHost=`cat /home/puneetkumar/cred.txt  | grep 'DST_HOST' | cut -d '"' -f 2`
+SlaveUser=`cat /home/puneetkumar/cred.txt | grep 'DST_USER' | cut -d '"' -f 2`
+SlavePwd=`cat /home/puneetkumar/cred.txt | grep 'DST_PASSWORD' | cut -d '"' -f 2`
+threshold=300
+
+
+# Function to send message to Google Chat
+send_message_to_google_space() {
+  local webhook_url="https://chat.googleapis.com/v1/spaces/AAAAyA6PNOM/messages?key=AIzaSyDdI0hCZtE6vySjMm-WEfRq3CPzqKqqsHI&token=RBz7WgmsIZk_-16l7TRqtdI-b-8VCn4WtizewOVb7Hg"
+  local message="$1"
+
+  curl -X POST -H "Content-Type: application/json" -d "{\"text\": \"$message\"}" "$webhook_url"
+}
+
+alert_message="📢 🔴 MySQL replication  \u0042 \u0052 \u004F \u004B \u0045 \u004E 🔴 📢"
+alert_message+="\n\nHost IP : $SlaveHost"
+
+###################################################################################
+#Grab the lines for each and use Gawk to get the last part of the string(Yes/No)  #
+###################################################################################
+
+SQLresponse=`mysql -h$SlaveHost -u$SlaveUser -p$SlavePwd mysql -e "show slave status \G" |grep -i "Slave_SQL_Running"|gawk '{print $2}'`
+IOresponse=`mysql -h$SlaveHost -u$SlaveUser -p$SlavePwd mysql -e "show slave status \G" |grep -i "Slave_IO_Running"|gawk '{print $2}'`
+Secondsbehind=`mysql -h$SlaveHost -u$SlaveUser -p$SlavePwd mysql -e "show slave status \G" |grep -i "Seconds_Behind_Master"|gawk '{print $2}'`
+
+if [ "$SQLresponse" = "No" ]; then
+
+error="Replication on the slave MySQL server($SlaveHost) has stopped working. Slave_SQL_Running: No"
+status=1
+fi
+
+if [ "$IOresponse" = "No" ]; then
+    error="Replication on the slave MySQL server($SlaveHost) has stopped working. Slave_IO_Running: No"
+        status=1
+fi
+
+if [[ $Secondsbehind -gt $threshold ]]; then
+error="Slave MySQL server($SlaveHost) has a Lag. of : $Secondsbehind Seconds "
+status=1
+fi
+##########################################
+# If the replication is not working      #
+##########################################
+
+if [ $status = 1 ]; then
+
+      send_alert=1
+alert_message+="\n\n $error"
+fi
+
+if [[ $send_alert -eq 1 ]];
+then
+    send_message_to_google_space "$alert_message"
+fi
+
+
